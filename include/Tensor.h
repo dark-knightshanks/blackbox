63939c448d88d9238015e483ad41c82363a2ca25
knight <rishirajrajgor@gmail.com>
2026-07-14 13:35:56 +0530

    updated tensor class

--- include/Tensor.h
+++ include/Tensor.h
@@ -3,7 +3,14 @@
 
 class Tensor{
 public:
-std::vector<__INT64_TYPE__> shape;// shape of the weigths and images
+std::vector<int64_t> shape;// shape of the weigths and images
 std::vector<float> data; //stor all the data
-
+size_t size() const { // helps to calculate the total size 
+        size_t total = 1;
+        for(int i = 0; i<shape.size(); i++){
+            int64_t dim = shape[i];
+            total *= dim;
+        } // mutliplies all the dim and return total size
+        return total;
+    }
 };
